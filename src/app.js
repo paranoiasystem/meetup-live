b378cff2c5c19de08fb2259fd24d3f294ccbe6a9
Marco Ferraioli <marcoferraioli@live.com>
2023-10-19 19:38:48 +0200

    update

--- src/app.js
+++ src/app.js
@@ -12,6 +12,30 @@ const app = () => {
         reply.send({ result: eval(code) })
     })
 
+    fastify.get('/number/:num', async (request, reply) => {
+        const { num } = request.params
+        let result = false
+        if (num % 2 == 0) {
+            result = true
+        }
+        reply.send({ result: result })
+    })
+
+    fastify.get('/string/:str', async (request, reply) => {
+        const { str } = request.params
+        let length = 0
+        if (str.length === 0) {
+            length = 0
+        } else if(str.length === 1) {
+            length = 1
+        } else if(str.length === 2) {
+            length = 2
+        } else {
+            length = 3
+        }
+        reply.send({ length: length })
+    })
+
     return fastify
 }
 
---
+++ src/test/string.spec.js
@@ -0,0 +1,21 @@
+import t from 'tap'
+import app from '../app.js'
+
+t.test('GET `/insecure` route', async t => {
+    const fastify = app()
+
+    t.plan(3)
+
+    t.teardown(async () => {
+        await fastify.close()
+    })
+
+    const response = await fastify.inject({
+        method: 'GET',
+        url: '/string/a'
+    })
+
+    t.equal(response.statusCode, 200)
+    t.equal(response.headers['content-type'], 'application/json; charset=utf-8')
+    t.same(JSON.parse(response.payload), { length: 1 })
+})
