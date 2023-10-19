4aeaa194856c05150ae77019d58e4cf5b6d288a7
Marco Ferraioli <marcoferraioli@live.com>
2023-10-19 19:42:56 +0200

    update

--- src/test/number.spec.js
+++ src/test/number.spec.js
@@ -1,7 +1,7 @@
 import t from 'tap'
 import app from '../app.js'
 
-t.test('GET `/insecure` route', async t => {
+t.test('GET `/number` route #1', async t => {
     const fastify = app()
 
     t.plan(3)
@@ -18,4 +18,23 @@ t.test('GET `/insecure` route', async t => {
     t.equal(response.statusCode, 200)
     t.equal(response.headers['content-type'], 'application/json; charset=utf-8')
     t.same(JSON.parse(response.payload), { result: false })
+})
+
+t.test('GET `/number` route #2', async t => {
+    const fastify = app()
+
+    t.plan(3)
+
+    t.teardown(async () => {
+        await fastify.close()
+    })
+
+    const response = await fastify.inject({
+        method: 'GET',
+        url: '/number/2'
+    })
+
+    t.equal(response.statusCode, 200)
+    t.equal(response.headers['content-type'], 'application/json; charset=utf-8')
+    t.same(JSON.parse(response.payload), { result: true })
 })
--- src/test/string.spec.js
+++ src/test/string.spec.js
@@ -1,7 +1,7 @@
 import t from 'tap'
 import app from '../app.js'
 
-t.test('GET `/insecure` route', async t => {
+t.test('GET `/string` route #1', async t => {
     const fastify = app()
 
     t.plan(3)
@@ -18,4 +18,61 @@ t.test('GET `/insecure` route', async t => {
     t.equal(response.statusCode, 200)
     t.equal(response.headers['content-type'], 'application/json; charset=utf-8')
     t.same(JSON.parse(response.payload), { length: 1 })
+})
+
+t.test('GET `/string` route #2', async t => {
+    const fastify = app()
+
+    t.plan(3)
+
+    t.teardown(async () => {
+        await fastify.close()
+    })
+
+    const response = await fastify.inject({
+        method: 'GET',
+        url: '/string/ab'
+    })
+
+    t.equal(response.statusCode, 200)
+    t.equal(response.headers['content-type'], 'application/json; charset=utf-8')
+    t.same(JSON.parse(response.payload), { length: 2 })
+})
+
+t.test('GET `/string` route #3', async t => {
+    const fastify = app()
+
+    t.plan(3)
+
+    t.teardown(async () => {
+        await fastify.close()
+    })
+
+    const response = await fastify.inject({
+        method: 'GET',
+        url: '/string/abcd'
+    })
+
+    t.equal(response.statusCode, 200)
+    t.equal(response.headers['content-type'], 'application/json; charset=utf-8')
+    t.same(JSON.parse(response.payload), { length: 3 })
+})
+
+t.test('GET `/string` route #3', async t => {
+    const fastify = app()
+
+    t.plan(3)
+
+    t.teardown(async () => {
+        await fastify.close()
+    })
+
+    const response = await fastify.inject({
+        method: 'GET',
+        url: '/string/'
+    })
+
+    t.equal(response.statusCode, 200)
+    t.equal(response.headers['content-type'], 'application/json; charset=utf-8')
+    t.same(JSON.parse(response.payload), { length: 0 })
 })
